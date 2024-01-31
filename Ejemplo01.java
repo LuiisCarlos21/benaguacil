23e4d0f00fe22f3e7a9568f0df37ee294ed92c6b
LuiisCarlos21 <1623041@alu.murciaeduca.es>
2024-01-31 19:12:54 +0100

    LM10 Añadir bucle for al archivo Ejemplo01.java

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -3,6 +3,10 @@ public class Ejemplo01 {
 		System.out.println("Hola mundo!!");
 		String nombre = "Luis Carlos";
 		System.out.println(nombre);
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("Vuelta " + (i + 1));
+		}
 	}
 }
-
+ 
