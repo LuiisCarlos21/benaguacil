5ec7ba5b744881a71174f2168cd53c4e979c05b9
Luis Carlos Caicedo <158084800+LuiisCarlos21@users.noreply.github.com>
2024-01-31 19:11:10 +0100

    Update Ejemplo01.java

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -3,6 +3,7 @@ public class Ejemplo01 {
 		System.out.println("Hola mundo!!");
 		String nombre = "Luis Carlos";
 		System.out.println(nombre);
+		System.out.println("La suma de 2 + 3 es:" + (2+3));
 	}
 }
 
