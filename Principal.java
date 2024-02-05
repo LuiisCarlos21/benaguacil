99d5e620c25126cfc0b33ea5d1a208f96863943d
LuiisCarlos21 <1623041@alu.murciaeduca.es>
2024-02-05 19:36:30 +0100

    Principal.java y README.md modificado

--- Principal.java
+++ Principal.java
@@ -1,5 +1,17 @@
+import java.time.LocalDateTime;
+import java.util.Date;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!");
+		System.out.println(horaActual());
+	}
+	public static String horaActual() {
+		Date fecha = new Date();
+		LocalDateTime localDate = LocalDateTime.now();
+		int hours = localDate.getHour();
+		int minutes = localDate.getMinute();
+		int seconds = localDate.getSecond();	
+
+		return "Fecha actual: " + fecha;	
 	}
 }
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Entornos de Desarrollo 1ºDAW 
 
 ## Principal.java
-> Este fichero es un ejemplo que muestra un hola mundo.
+> Este fichero es un ejemplo que muestra un hola mundo. además muestra la fecha y hora actual.
+
 ### Otro apartado
 + Luis
 + Alberto
